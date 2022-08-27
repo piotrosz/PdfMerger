7bdfc5bdc0c57148e15d92a1d66557070bcf6fc9
Piotr <piotrosz@gmail.com>
2022-08-27 14:10:13 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using PdfSharpCore.Pdf;
+using PdfSharpCore.Pdf.IO;
+
+Console.WriteLine("Merge in progress...");
+
+var inputDirectory = args[0];
+string[] pdfFiles = GetPdfFiles(inputDirectory);
+
+using var outputDocument = new PdfDocument();
+
+foreach (string pdfFile in pdfFiles)
+{
+    PdfDocument inputDocument = PdfReader.Open(pdfFile, PdfDocumentOpenMode.Import);
+
+    for (int pageIndex = 0; pageIndex < inputDocument.PageCount; pageIndex++)
+    {
+        PdfPage page = inputDocument.Pages[pageIndex];
+        outputDocument.AddPage(page);
+    }
+}
+
+var outputDirectory = $"{inputDirectory}/output";
+Directory.CreateDirectory(outputDirectory);
+outputDocument.Save($"{outputDirectory}/merged.pdf");
+Console.WriteLine($"Done. Output file saved in {outputDirectory}");
+
+string[] GetPdfFiles(string inputDirectory)
+{
+    return Directory
+        .GetFiles(inputDirectory, "*.pdf")
+        .OrderBy(x => x)
+        .ToArray();
+}
